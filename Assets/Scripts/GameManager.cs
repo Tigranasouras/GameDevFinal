0aa0edad611ba9086bd7ae31b51229a4f5b3499d
Daron <daronbaltazar@gmail.com>
2025-04-29 17:43:15 -0500

    GameManager Script
    GameManager Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManagers Instance;
+
+    public int currentLevel = 1;
+    public int maxLevel = 3;
+    public bool inCombat = false;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // persist between scenes
+
+    }else{
+        Destroy(gameObject);
+    }
+
+    public void StartGame(){
+        currentLevel = 1;
+        LoadGame();
+    }
+
+    public void LoadGame(){
+        inCombat = true;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadSacrifice()
+    {
+        inCombat = false;
+        SceneManager.LoadScene("Sacrifice");     
+    }
+
+    public void NextLevel()
+    {      
+    currentLevel++;
+    if(currentLevel > maxLevel){
+    LoadVictory();
+    }else{
+    LoadSacrifice();
+    }
+    }
+
+    public void LoadVictory(){
+        SceneManager.LoadScene("VictoryScene");
+    }
+
+.
+
+
+
+
+
+}
